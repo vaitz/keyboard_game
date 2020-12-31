f5e05002908195b0627512b76f66c6509e4bdf97
vaitz <vaitz.post.bgu.ac.il>
2020-12-31 12:02:28 +0200

    update client and server

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import select
 from pynput.keyboard import Listener
 
 
+# Class for pretty colors prints
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -16,73 +17,75 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+
 BROADCAST_PORT = 13117
 
-
+# Press keys handlers
 def on_press(key):
     pass
 
 
 def on_release(key):
     print(key)
+    # try:
     client_tcp.sendall(key.char.encode())
-
+    # except ConnectionError:
+    #     pass
+    # except OSError:
+    #     pass
 
 # CLIENT MODES:
 # Looking for a server
 def lookup_server(client_udp):
     server_ip, server_port = '', 0
     while True:
+        # search for broadcast
         data, address = client_udp.recvfrom(1024)
         (ip, port) = address
         server_ip = ip
-        # try catch???
-        (magic_cookie, msg_type, server_port) = struct.unpack('Ibh', data)
+        # try to unpack the message
+        try:
+            (magic_cookie, msg_type, server_port) = struct.unpack('Ibh', data)
+        except struct.error:
+            continue
         magic_cookie = hex(magic_cookie)
         msg_type = hex(msg_type)
-        # print(magic_cookie, msg_type, server_port)
+
         # check the UDP message format
-        if magic_cookie != '0xfeedbeef' or msg_type != '0x2':
-            print(bcolors.WARNING + "UDP message not in format!" + bcolors.ENDC)
+        if magic_cookie != "0xfeedbeef" or msg_type != "0x2" or server_port < 0:
             continue
 
-        # need to check if there are more data after the port???????????????????????
-        print("Received offer from " + ip + ", attempting to connect...")
+        print(bcolors.OKGREEN + "Received offer from " + ip + ", attempting to connect..."+ bcolors.ENDC)
         break
     return server_ip, server_port
 
 
 # Connecting to a server
 def connect(server_ip, server_port, client_tcp):
-    # socket.settimeout() ?? if not accepted
-    try:
+    # try:
         client_tcp.connect((server_ip, server_port))
-        client_tcp.sendall('Team1\n'.encode())
+        client_tcp.sendall('Key2Pay\n'.encode())
         return True
-    except ConnectionError:
-        return False
+    # except ConnectionError:
+    #     return False
 
 
 # Game mode
 def game(client_tcp):
-    # start game
+    # waiting for start game message
     data = client_tcp.recv(1024)
     print(data.decode())
 
-    # ?????
-    with Listener(on_press=on_press, on_release=on_release) as listener:  # Create an instance of Listener
-        # listener.join()
-        while True:
-            # end game
-            data = client_tcp.recv(1024)
-            print(data.decode())
-            listener.stop()
-            break
-
+    # thread listener, recives characters input from the user
+    with Listener(on_press=on_press, on_release=on_release) as listener:  
+        # waiting for end game message and stop listening to typing
+        data = client_tcp.recv(1024)
+        print(data.decode())
+        listener.stop()
 
     print("Server disconnected, listening for offer requests...")
 
-
+# Client's main loop - waiting, connect, game
 print(bcolors.OKGREEN + "Client started, listening for offer requests..." + bcolors.ENDC)
 while True:
     # create UDP socket for lookup
@@ -90,15 +93,24 @@ while True:
     client_udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)# Enable broadcasting mode
     client_udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     client_udp.bind(("", BROADCAST_PORT))
+
+    # waiting mode
     server_ip, server_port = lookup_server(client_udp)
     client_udp.close()
+    
     # create TCP socket for lookup
     client_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # take care of case that connect to bad server or connection failed
+    # client_tcp.settimeout(10)
+    # try:
     ans = connect(server_ip, server_port, client_tcp)
     if ans:
         game(client_tcp)
     client_tcp.close()
+    # except socket.timeout:
+    #     pass
+    # except ConnectionError:
+    #     pass
 
 
 
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import time
 import threading
 import struct
 
+# Class for pretty colors prints
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -15,15 +16,24 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+# Global variables:
+
+# Addresses
+name = socket.gethostname()
+SERVER_ADDRESS = socket.gethostbyname(name)
 BROADCAST_ADDRESS = "255.255.255.255"
+SERVER_PORT = 2034
+
+# UDP message format
 BROADCAST_PORT = 13117
 MAGIC_COOKIE = 0xfeedbeef
 MSG_TYPE = 0x2
-SERVER_PORT = 2034
-global list_threads, list_sockets
+
+# Game variables
+global list_threads, list_sockets, team_names, game_on
 list_threads = []
 list_sockets = []
-team_names = []
+team_names = {}
 score_group1 = 0
 score_group2 = 0
 group1 = []
@@ -31,79 +41,77 @@ group2 = []
 game_on = False
 
 
+# UDP broadcast handler - thread
 def udp_broadcast(server_udp):
+    # ten seconds interval - sends udp message every 1 second
     stop_time = time.time() + 10
     while time.time() < stop_time:
         data = struct.pack('Ibh', MAGIC_COOKIE, MSG_TYPE, SERVER_PORT)
         server_udp.sendto(data, (BROADCAST_ADDRESS, BROADCAST_PORT))
-        print("send")
         time.sleep(1)
 
-
+# TCP clients handler - thread
 def client_thread(client_socket_conn):
-    # client_socket_conn.sendall('welcome to the server, send your team name please')
+    # getting the group name
     data = client_socket_conn.recv(1024)
     team_name = data.decode().split('\n')[0]
-    print(team_name)
-    team_names.append(team_name)
+    team_names[team_name] = 0
+    
+    # try:
+    while True:
+        # waiting for the game to start
+        while game_on:
+            data = client_socket_conn.recv(1024)
+            if not data:
+                break
+            print(data.decode())
+            team_names[team_name] = team_names[team_name] + 1
+        # time.sleep(1)
+    client_socket_conn.close()
+    # except ConnectionError:
+    #     pass
 
-    run = True
-    counter = 0
-    # game
-    try:
-        while run:
-            if game_on:
-                while game_on:
-                    data = client_socket_conn.recv(1024)
-                    if not data:
-                        break
-                    print(data.decode())
-                    counter += len(data.decode())
-        client_socket_conn.close()
-    except ConnectionError:
-        pass
-
-
+# TCP handler - thread
 def tcp_listener(server_tcp):
+    # ten seconds interval - accepting connections and creating thread to each client
     stop_time = time.time() + 10
     while time.time() < stop_time:
-        (conn, address) = server_tcp.accept()
-        ct = threading.Thread(target=client_thread, args=(conn,))
-        list_threads.append(ct)
-        list_sockets.append(conn)
-        ct.run()
+        try:
+            (conn, address) = server_tcp.accept()
+            # conn.settimeout(10)
+            ct = threading.Thread(target=client_thread, args=(conn,))
+            list_threads.append(ct)
+            list_sockets.append(conn)
+            ct.run()
+        except ConnectionError:
+            break
+        except socket.timeout:
+            break
+        except OSError:
+            break
 
 
-# Waiting for clients
+# Sending broadcasts and waiting for clients
 def waiting(server_udp, server_tcp):
-    # stop_time = time.time() + 10
     T_UDP = threading.Thread(target=udp_broadcast, args=(server_udp,))
-
     T_TCP = threading.Thread(target=tcp_listener, args=(server_tcp,))
     T_UDP.start()
     T_TCP.start()
-    T_TCP.join(10)
-    # T_TCP.join()
-    # while time.time() < stop_time:
-        # accept connections from outside
-        # add timeout after 10 seconds without requests?????
-        # (conn, address) = server_tcp.accept()
-        # ct = threading.Thread(target=client_thread, args=(conn,))
-        # list_threads.append(ct)
-        # ct.run()
+    # Waits in this line for 10 seconds
+    T_UDP.join(10)
 
 # Game mode
 def game(server_tcp):
-    print(bcolors.HEADER+"Welcome to Keyboard Spamming Battle Royale"+bcolors.ENDC)
-    global score_group1, score_group2, group1, group2, game_on, list_threads, list_sockets
+    # initial the game variables
+    global score_group1, score_group2, group1, group2, list_threads, list_sockets, game_on
     score_group1 = 0
     score_group2 = 0
-    # split to 2 groups
     group1 = []
     group2 = []
     i = 1
     group1_str = ""
     group2_str = ""
+    # split to 2 groups
     for team in team_names:
         if i % 2 == 0:
             group1.append(team)
@@ -116,15 +124,22 @@ def game(server_tcp):
     # start game msg
     msg = "Welcome to Keyboard Spamming Battle Royale\nGroup 1:\n=======\n" + group1_str \
           + "Group 2:\n=======\n" + group2_str + "Start pressing keys on your keyboard as fast as you can!!"
+    print(msg)
     for socket in list_sockets:
         try:
             socket.sendall(msg.encode())
         except ConnectionError:
             pass
-
     game_on = True
+    # ten seconds for typing
     time.sleep(10)
     game_on = False
+
+    # sums the scores
+    for player1 in group1:
+        score_group1 += team_names[player1]
+    for player2 in group2:
+        score_group2 += team_names[player2]
 
     # end game msg
     msg_end = "Game over!\nGroup 1 typed in " + str(score_group1) + " characters. Group 2 typed in " \
@@ -133,47 +148,46 @@ def game(server_tcp):
         msg_end += ("\nGroup 1 wins!\nCongratulations to the winners:\n==============================\n" + group1_str)
     else:
         msg_end += ("\nGroup 2 wins!\nCongratulations to the winners:\n==============================\n" + group2_str)
-    for socket in list_sockets:
+    print(msg_end)
+
+    # updating the clients with game summarize
+    for socketc in list_sockets:
         try:
-            socket.sendall(msg_end.encode())
+            socketc.sendall(msg_end.encode())
+            socketc.close()
         except ConnectionError:
             pass
 
-    # kill list_threads
-
-    #
-    # print(bcolors.HEADER+"Game over!"+bcolors.ENDC)
-    # print("Group 1 typed in " + str(group1) + " characters. Group 2 typed in " + str(group2) +  "characters.")
-    # if score_group1 > score_group2:
-    #     print("Group 1 wins!")
-    #     print("Congratulations to the winners:")
-    #     print("==============================")
-    #     for name in group1:
-    #         print(name)
-    # else:
-    #     print("Group 2 wins!")
-    #     print("Congratulations to the winners:")
-    #     print("==============================")
-    #     for name in group2:
-    #         print(name)
-    print("Game over, sending out offer requests...")
+    print(bcolors.OKBLUE + "Game over, sending out offer requests..." + bcolors.ENDC)
 
 
-
-print(bcolors.OKBLUE + "Server started, listening on IP address 10.100.102.10" + bcolors.ENDC)
+# Server's main loop - waiting, game
+print(bcolors.OKBLUE + "Server started, listening on IP address " + SERVER_ADDRESS + "" + bcolors.ENDC)
 while True:
+    # initaial the clients variable
     list_threads = []
     list_sockets = []
+    team_names = {}
+
+    # create TCP socket for connecting clients
+    server_TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server_TCP.bind((SERVER_ADDRESS, int(SERVER_PORT)))
+    except OSError:
+        # time.sleep(1)
+        continue
+
     # create UDP socket for broadcast
     server_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # , socket.IPPROTO_UDP)
     server_UDP.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # Enable broadcasting mode
-    # create TCP socket for connecting clients
-    server_TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_TCP.bind(("10.100.102.10", int(SERVER_PORT)))
+    
     server_TCP.listen()
+    # server_TCP.settimeout(10)
+
     waiting(server_UDP, server_TCP)
     server_TCP.close()
     server_UDP.close()
+
     game(server_TCP)
 
 
